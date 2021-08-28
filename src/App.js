76fbe8148ccbb96ff8b50aebc34ebc4c95764002
chanelclare05 <chaneldonahoe@hotmail.com>
2021-08-28 20:52:23 +1000

    style color box

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react'
+import './App.css';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+          <Palette {...seedColors[2]}/>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
---
+++ src/Palette.js
@@ -0,0 +1,22 @@
+// import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+import ColorBox from './ColorBox';
+import './Palette.css'
+
+
+export default class Palette extends Component {
+    render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+        ));
+        return( 
+            <div className='Palette'>
+                {/* navbar  goes here */}
+                <div className='Palette-colors'>  
+                    {colorBoxes}
+                </div>
+                {/* footer goes here */}
+            </div>
+        )
+    }
+} 
