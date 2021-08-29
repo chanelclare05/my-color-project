3b0cf889dbfc8cb579bab4dd4969f7ecd363a24c
chanelclare05 <chaneldonahoe@hotmail.com>
2021-08-29 12:00:24 +1000

    added style change in handlebar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import seedColors from './seedColors';
 
 class App extends Component {
   render(){
-    console.log(generatePalette(seedColors[4]))
+
     return (
       <div className="App">
           <Palette palette={generatePalette(seedColors[4])}/>
--- src/Palette.js
+++ src/Palette.js
@@ -1,40 +1,37 @@
-// import { render } from '@testing-library/react'
 import React, { Component } from 'react'
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css'
-
-import Slider, { Range } from 'rc-slider';
-import 'rc-slider/assets/index.css';
-
-
 
 export default class Palette extends Component {
     constructor(props) {
         super(props);
-        this.state = { level: 500 }
+        this.state = { level: 500, format:'hex' };
         this.changeLevel = this.changeLevel.bind(this);
+        this.changeFormat=this.changeFormat.bind(this);
     }
 
     changeLevel(level){
-        this.setState({level})
+        this.setState( { level })
+    }
+
+    changeFormat(val){
+        this.setState({format: val})
     }
 
     render() {
         const {colors} = this.props.palette;
-        const { level } = this.state;
+        const { level, format } = this.state;
         const colorBoxes = colors[level].map(color => (
-            <ColorBox background={color.hex} name={color.name} />
+            <ColorBox background={color[format]} name={color.name} />
         ));
         return (
             <div className='Palette'>
-                <Slider 
-                    defaultValue={level} 
-                    min={100} 
-                    max={900} 
-                    step={100}
-                    onAfterChange={this.changeLevel} 
-                />
-                {/* navbar  goes here */}
+                <Navbar 
+                    level={level}
+                    changeLevel={this.changeLevel}
+                    handleChange={this.changeFormat}
+                />   
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
